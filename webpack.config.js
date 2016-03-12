9e997e9f184a789248e26a5675ac61a26aeca052
Seth Bergman <seth.atxwebs@gmail.com>
2016-03-12 14:39:13 -0600

    iocloudstack deploy

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ var config = {
 };
 
 if (process.env.NODE_ENV === 'production') {
-  config.output.path = __dirname + '/dist';
+  config.output.path = __dirname + '/';
   config.plugins.push(new webpack.optimize.UglifyJsPlugin());
   config.devtool = 'source-map';
 }
